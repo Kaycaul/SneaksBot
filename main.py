5e2fe7a84667cd620a92044ae79c084cefa86f1f
Doe <@Doeball#0409>
2022-12-10 01:11:18 -0500

    shit

--- main.py
+++ main.py
@@ -68,7 +68,7 @@ async def chain_message(message: discord.Message):
       return
   # contribute to the spam
   print(f"Spamming {message.content}")
-  await message.channel.send(
+  await message.reply(
     content=message.content,
     reference=message.to_reference(),
     mention_author=True
