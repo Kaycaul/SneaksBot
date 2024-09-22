0d01c7f65cbcbc686ccf6d40456a875ac75a25fc
Doeball <doughcatball@gmail.com>
2024-09-22 13:12:42 -0400

    entire try block for upload because i dont wanna fix it

--- main.py
+++ main.py
@@ -62,47 +62,50 @@ async def on_message(message: discord.Message):
 @app_commands.describe(artist="name of the artist")
 @app_commands.describe(tags="space-separated tags")
 async def post(interaction, url: str, artist: str, tags: str):
-    # break if user doesnt have doeball uid
-    if interaction.user.id != sneaksbot.doeball_uid:
-        await interaction.response.send_message("<:sneakers:1064268113434120243>❌")
-        return
-    # save image to the servers folder (stupid design because sneaksbot wasnt messy enough)
-    root = os.environ.get("MEOW_DOEBALL_CA_ROOT_FOLDER")
-    # regex the image name at the end of the url
-    directory = "/public/assets/uploads/"
-    file = os.path.basename(url).split("?")[0]
-    save_path = f"{root}{directory}{file}"
-    print(f"saving the image at {url} to {save_path}")
-    # never overwrite
-    if (os.path.exists(save_path)):
-        await interaction.response.send_message("file already exists dumbass rename it or something <:sneakers:1064268113434120243>❌")
-        return
-    with open(save_path, "wb") as handle:
-        try:
-            content = requests.get(url).content
-            handle.write(content)
-        except Exception as e:
-            await interaction.response.send_message(f"<:sneakers:1064268113434120243>❌ exception accessing url:\n`{e}`")
-            os.remove(save_path)
-            return
     try:
-        # with the new path, put the image path in the database with the metadata
-        taglist = tags.split(" ")
-        newpost = {
-            "path": f"/assets/uploads/{file}",
-            "artist": artist,
-            "tags": taglist,
-            "date": datetime.now(timezone.utc)
-        }
-        _id = artworks_collection.insert_one(newpost)
-        res = f"by {artist}\ntags: {taglist}\nInserted @ {_id.inserted_id}\nhttps://meow.doeball.ca/gallery/{_id.inserted_id}\n<:sneakers:1064268113434120243>✅"
-        await interaction.response.send_message(res)
+        # break if user doesnt have doeball uid
+        if interaction.user.id != sneaksbot.doeball_uid:
+            await interaction.response.send_message("<:sneakers:1064268113434120243>❌")
+            return
+        # save image to the servers folder (stupid design because sneaksbot wasnt messy enough)
+        root = os.environ.get("MEOW_DOEBALL_CA_ROOT_FOLDER")
+        # regex the image name at the end of the url
+        directory = "/public/assets/uploads/"
+        file = os.path.basename(url).split("?")[0]
+        save_path = f"{root}{directory}{file}"
+        print(f"saving the image at {url} to {save_path}")
+        # never overwrite
+        if (os.path.exists(save_path)):
+            await interaction.response.send_message("file already exists dumbass rename it or something <:sneakers:1064268113434120243>❌")
+            return
+        with open(save_path, "wb") as handle:
+            try:
+                content = requests.get(url).content
+                handle.write(content)
+            except Exception as e:
+                await interaction.response.send_message(f"<:sneakers:1064268113434120243>❌ exception accessing url:\n`{e}`")
+                os.remove(save_path)
+                return
+        try:
+            # with the new path, put the image path in the database with the metadata
+            taglist = tags.split(" ")
+            newpost = {
+                "path": f"/assets/uploads/{file}",
+                "artist": artist,
+                "tags": taglist,
+                "date": datetime.now(timezone.utc)
+            }
+            _id = artworks_collection.insert_one(newpost)
+            res = f"by {artist}\ntags: {taglist}\nInserted @ {_id.inserted_id}\nhttps://meow.doeball.ca/gallery/{_id.inserted_id}\n<:sneakers:1064268113434120243>✅"
+            await interaction.response.send_message(res)
+        except Exception as e:
+            # something went wrong inserting the document
+            await interaction.response.send_message(f"<:sneakers:1064268113434120243>❌ exception inserting document:\n`{e}`")
+            # delete the file
+            if os.path.exists(save_path):
+                os.remove(save_path)
     except Exception as e:
-        # something went wrong inserting the document
-        await interaction.response.send_message(f"<:sneakers:1064268113434120243>❌ exception inserting document:\n`{e}`")
-        # delete the file
-        if os.path.exists(save_path):
-            os.remove(save_path)
+        await interaction.response.send_message(f"<:sneakers:1064268113434120243>❌ exception:\n`{e}`")
 
 # keep_alive()  # Starts a webserver to be pinged.
 token = os.environ.get("DISCORD_BOT_SECRET")
